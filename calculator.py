09b9e72e9cc77199c7dd66f7243541bd926e3be7
seyer <seyer@qq.com>
2017-11-20 18:28:21 +0800

    change

--- calculator.py
+++ calculator.py
@@ -11,6 +11,12 @@ class Config(object):
                 self._config[cf_line[0].strip(' ')]=float(cf_line[1])
     def get_config(self,key_name):
         return self._config[key_name]
+    def get_JiShuL(self):
+        return self.get_config('JiShuL')
+    def get_JiShuH(self):
+        return self.get_config('JiShuH')
+    def get_taxrate():
+        return sum([self.get_config('YangLao'),self.get_config('YiLiao'),self.get_cofnig('ShiYe'),self.get_config('GongShang'),self.get_config('ShengYu'),self.get_config('GongJiJin')])
 ##员工数据类
 class UserData(object):
     def __init__(self,userdatafile):
@@ -21,41 +27,11 @@ class UserData(object):
                 self.userdata[ud_line[0].strip(' ')]=int(float(ud_line[1]))
             
     ##税后工资计算函数         
-    def calculator(self,config_class):
+    def calculator(self):
+        salary_list=[]
         for ud in self.userdata:
-            if self.userdata[ud]<=self.config.get_config('JiShuL'):
-                self.
-            
-        if self.tax_payable<=1500 and self.tax_payable>0:
-            self.salary=self.salary-self.social_taxs-self.tax_payable*0.03
-        elif self.tax_payable>1500 and self.tax_payable<=4500 :
-            self.salary=self.salary-self.social_taxs-(self.tax_payable*0.1-105)
-        elif self.tax_payable>4500 and self.tax_payable<=9000 :
-            self.salary=self.salary-self.social_taxs-(self.tax_payable*0.2-555)
-        elif self.tax_payable>9000 and self.tax_payable<=35000 :
-            self.salary=self.salary-self.social_taxs-(self.tax_payable*0.25-1005)
-        elif self.tax_payable>35000 and self.tax_payable<=55000 :
-            self.salary=self.salary-self.social_taxs-(self.tax_payable*0.3-2755)
-        elif self.tax_payable>55000 and self.tax_payable<=80000 :
-            self.salary=self.salary-self.social_taxs-(self.tax_payable*0.35-5505)
-        elif self.tax_payable>80000 :
-            self.salary=self.salary-self.social_taxs-(self.tax_payable*0.45-13505)
-        else:
-            self.salary=self.salary-self.social_taxs
-        
-
-    ##计算社保金额函数
-    def cal_social(self):
-        
-        if 0<self.userdata[ud]<=config.get_config('JiShuL'):
-            self.social_taxs=config.get_config('JiShuL')*(0.08+0.02+0.005+0+0+0.06)
-        elif config.get_config('JiShuL')<self.salary<config.get_config('JiShuH'):
-            self.social_taxs=self.salary*(0.08+0.02+0.005+0+0+0.06)
-        elif config.get_config('JiShuH')<=self.salary:
-            self.socail_taxs=16446*(0.08+0.02+0.005+0+0+0.06)
-        self.ta_payable=self.salary-self.socail_taxs
-        
-        
+            if self.userdata[ud]<=config.get_JiShuL():
+                
     ##计算个税金额
         
     ##计算税后工资
@@ -65,16 +41,21 @@ class UserData(object):
             self.userdata[ud]
             self.calculator()
             with open(outputfile,'a') as file:
-                file.write(str(ud)+','+str(self.userdate[ud])+','+'/n')
+                file.write(str(ud)+','+str(self.userdata[ud])+','+'/n')
 ##主程序
 if __name__=='__main__':
     if len(sys.argv)>0 :
         args=sys.argv[1:]
         index=args.index('-c')
         configfile=args[index+1]
+        config=Config(configfile)
         index=args.index('-d')
         userdatefile=args[index+1]
-        config=Config(configfile)
+        index=args.index('-o')
+        outputfile=args[index+1]
         userdata=UserData(userdatefile)
-        userdata.cal_social(config)
+        userdata.calculator()
+        
+
+        userdata.dumptofile(outputfile)
         
