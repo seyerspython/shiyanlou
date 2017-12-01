9b459f28e6e09872e029d20a8bd1f8e78189cbbe
seyer <seyer@qq.com>
2017-12-02 00:39:56 +0800

    12.2

--- news/app.py
+++ news/app.py
@@ -1,9 +1,40 @@
 #!/usr/bin/env python3
 import os,json
 from flask import Flask,render_template,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root@localhost/shiyanlou'
+db=SQLAlchemy(app)
 
+class File(db.Model):
+	id=db.Column(db.Integer,primary_key=True)
+	title=db.Column(db.String(80))
+	created_time=db.Column(db.DateTime)
+	category_id=db.Column(db.Integer,db.ForeignKey('category.id'))
+	category=db.relationship('Category')
+	content=db.Column(db.Text)
+
+	def __init__(self,title,created_time,category_id,content):
+		self.title=title
+		self.created_time=created_time
+		self.category_id=category_id
+		self.content=content
+
+	def __repr__(self):
+		return '<File %r>' % self.title
+
+class Category(db.Model):
+	id=db.Column(db.Integer,primary_key=True)
+	name=db.Column(db.String(80))
+
+	def __init__(self,name):
+		self.name=name
+
+	def __repr__(self):
+		return '<Category %r>' % self.name
 @app.route('/')
 def index():
 	article_list=[]
